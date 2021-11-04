3e886c2e80d68247fd2c74555812183558026a17
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-04 15:39:27 -0300

    Método PUT no Simulador Controller

--- src/main/java/br/com/zup/SimuladorDeInvestimento/SimuladorController.java
+++ src/main/java/br/com/zup/SimuladorDeInvestimento/SimuladorController.java
@@ -1,2 +1,20 @@
-package br.com.zup.SimuladorDeInvestimento;public class SimuladorController {
+package br.com.zup.SimuladorDeInvestimento;
+
+import br.com.zup.SimuladorDeInvestimento.DTO.InvestimentoDTO;
+import br.com.zup.SimuladorDeInvestimento.DTO.SimuladorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/investir")
+public class SimuladorController {
+    @Autowired
+    private SimuladorService simuladorService;
+
+
+    @PutMapping
+    public SimuladorDTO simularInvestimento(@RequestBody InvestimentoDTO investimentoDTO){
+       SimuladorDTO simuladorDTO = simuladorService.calcularSimulacao(investimentoDTO);
+       return simuladorDTO;
+    }
 }
