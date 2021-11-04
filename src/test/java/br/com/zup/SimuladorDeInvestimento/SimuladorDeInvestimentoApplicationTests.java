b76d0dea6799f029c42d1c7a8629b16b3f4fa52d
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-04 11:08:53 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/zup/SimuladorDeInvestimento/SimuladorDeInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.SimuladorDeInvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDeInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
