7029241ee6695605c9c9e85f97b40b9f555ce493
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-04 11:42:19 -0300

    Adicionar valores da taxa de retorno

--- src/main/java/br/com/zup/SimuladorDeInvestimento/Risco.java
+++ src/main/java/br/com/zup/SimuladorDeInvestimento/Risco.java
@@ -1,5 +1,16 @@
 package br.com.zup.SimuladorDeInvestimento;
 
 public enum Risco {
-    ALTO, MEDIO, BAIXO
+    ALTO(2.5), MEDIO(0.0015), BAIXO(0.0005);
+    private double taxaRetorno;
+
+    Risco(double taxaRetorno) {
+        this.taxaRetorno = taxaRetorno;
+    }
+
+    public double getTaxaRetorno() {
+        return taxaRetorno;
+    }
+
+
 }
